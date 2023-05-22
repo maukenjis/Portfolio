f600223b670d677837a128f1fb07e612fce215c6
maukenjis <134302677+maukenjis@users.noreply.github.com>
2023-05-22 16:29:20 -0300

    Add files via upload

---
+++ js/JS_main_portf.js
@@ -0,0 +1,5 @@
+function easterEgg(){
+			console.log("There's nothing here! Maybe scroll down?");
+			let kanna=document.getElementsByClassName('easterEgg')[0];
+			kanna.style.display="inline";
+		}
