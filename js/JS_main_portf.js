a3f33ca9a6a4dc55023de5e168eda1be33d87afe
maukenjis <134302677+maukenjis@users.noreply.github.com>
2023-05-22 21:50:08 -0300

    Add files via upload

--- js/JS_main_portf.js
+++ js/JS_main_portf.js
@@ -3,3 +3,25 @@ function easterEgg(){
 			let kanna=document.getElementsByClassName('easterEgg')[0];
 			kanna.style.display="inline";
 		}
+
+function Bounce(x){
+	let brand1="linkedin";
+	let brand2="github";
+	if(x==brand1){
+		document.getElementsByClassName("fa-brands")[0].setAttribute("class","fa-brands fa-bounce fa-"+"linkedin");
+	}
+	else{
+		document.getElementsByClassName("fa-brands")[1].setAttribute("class","fa-brands fa-bounce fa-"+"github");
+	}
+}
+
+function Unbounce(x){
+	let brand1=document.getElementsByClassName("fa-brands")[0].getAttribute("id");
+	let brand2=document.getElementsByClassName("fa-brands")[1].getAttribute("id");
+	if(x==brand1){
+		document.getElementsByClassName("fa-brands")[0].setAttribute("class","fa-brands fa-"+"linkedin");
+	}
+	else{
+		document.getElementsByClassName("fa-brands")[1].setAttribute("class","fa-brands fa-"+"github");
+	}
+}
